26be83fdc1841e6d251e40c9dc374a8a06cfd5b9
tusharpandey13 <tusharpandey13@gmail.com>
2021-06-30 12:47:35 +0530

    tushar 12:47:35

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ port = process.env.PORT || 8080;
 
 // Catch all other routes and return the index file
 app.get('/', (req, res) => {
-  res.send(process.env.NODE_ENV);
+  res.send(process.env);
 });
 
 // use morgan to log requests to the console
